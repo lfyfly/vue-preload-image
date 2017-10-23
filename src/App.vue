29d4b4327082cadc7e0f234b59bf07c0303929c8
lfyfly <410793635@qq.com>
2017-10-23 10:08:08 +0800

    添加移动端兼容样式

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-#app
+#app(:class="{'is-mobile':isMobile}")
   preload-image(:imgUrlArr="imgUrls", @imgAllLoaded="fn")
 
   .imgs-container(v-if="preLoaded")
@@ -17,10 +17,10 @@ export default {
   },
   data() {
     return {
-      imgUrls: ['static/img/1.jpg', 'static/img/2.jpg', 'static/img/3.jpg', 'static/img/4.jpg', 'static/img/5.jpg', 'static/img/6.jpg', 'static/img/7.jpg', 'static/img/8.jpg', 'static/img/9.jpg', 'static/img/10.jpg','static/img/11.jpg', 'static/img/12.jpg'],
+      imgUrls: ['static/img/1.jpg', 'static/img/2.jpg', 'static/img/3.jpg', 'static/img/4.jpg', 'static/img/5.jpg', 'static/img/6.jpg', 'static/img/7.jpg', 'static/img/8.jpg', 'static/img/9.jpg', 'static/img/10.jpg', 'static/img/11.jpg', 'static/img/12.jpg'],
 
-      // imgUrls: ['static/img/1.jpg', 'static/img/2.jpg', 'static/img/3.jpg', 'static/img/4.jpg', 'static/img/5.jpg', 'static/img/6.jpg', 'static/img/7.jpg', 'static/img/8.jpg', 'static/img/9.jpg', 'static/img/10.jpg', 'static/img/11.jpg', 'static/img/12.jpg', 'static/img/13.jpg', 'static/img/14.jpg', 'static/img/15.jpg', 'static/img/16.jpg', 'static/img/17.jpg', 'static/img/18.jpg', 'static/img/19.jpg', 'static/img/20.jpg'],
-      preLoaded: false // 预加载是否完成
+      preLoaded: false, // 预加载是否完成
+      isMobile: !!navigator.userAgent.match(/(iPhone|iPod|Android|ios)/i)
     }
   },
   methods: {
@@ -42,8 +42,17 @@ body {
 
 #app {
   .imgs-container {
+  background: #000;
+
     img {
       width: 25%;
+    }
+  }
+  &.is-mobile {
+    .imgs-container {
+      img {
+        width: 50%;
+      }
     }
   }
 }
